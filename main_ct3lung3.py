d7a18511b78ffd084897b921bb2fb0facb399f08
Tran Minh Quan <quantm3@sdc2-hpe02.vinbrain.net>
2020-08-22 21:09:32 +0700

    3D Lung Segmentation

--- main_ct3lung3.py
+++ main_ct3lung3.py
@@ -8,10 +8,12 @@ from collections import OrderedDict
 from natsort import natsorted
 from tqdm import tqdm
 
+import random
 import numpy as np
-import cv2
+import cv2, skimage.io, skimage.transform, skimage.exposure
 import albumentations as AB
-
+# import volumentations as VL
+import torchio as TI
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -29,10 +31,12 @@ from pytorch_lightning.loggers import WandbLogger
 
 import kornia
 
-train_image_dir = ['/vinbrain/data/iXrayCT_COVID/data/train/xr2lung2/NLMMC/neg/images/']
-train_label_dir = ['/vinbrain/data/iXrayCT_COVID/data/train/xr2lung2/NLMMC/neg/labels/']
-valid_image_dir = ['/vinbrain/data/iXrayCT_COVID/data/test/xr2lung2/JSRT/neg/images/']
-valid_label_dir = ['/vinbrain/data/iXrayCT_COVID/data/test/xr2lung2/JSRT/neg/labels/']
+train_image_dir = ['/u01/data/iXrayCT_COVID/data/train/ct3lung3/NSCLC/neg/images/']
+train_label_dir = ['/u01/data/iXrayCT_COVID/data/train/ct3lung3/NSCLC/neg/labels/']
+valid_image_dir = ['/u01/data/iXrayCT_COVID/data/test/ct3lung3/MEDSEG/pos/images/', 
+                   '/u01/data/iXrayCT_COVID/data/test/ct3lung3/NSCLC/neg/images/']
+valid_label_dir = ['/u01/data/iXrayCT_COVID/data/test/ct3lung3/MEDSEG/pos/labels/', 
+                   '/u01/data/iXrayCT_COVID/data/test/ct3lung3/NSCLC/neg/labels/', ]
 
 # based on:
 # https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
@@ -46,11 +50,11 @@ def dice_loss(input, target, eps=1e-8):
         raise TypeError("Input type is not a torch.Tensor. Got {}"
                         .format(type(input)))
 
-    if not len(input.shape) == 4:
+    if not len(input.shape) >= 4:
         raise ValueError("Invalid input shape, we expect BxNxHxW. Got: {}"
                          .format(input.shape))
 
-    if not input.shape[-2:] == target.shape[-2:]:
+    if not input.shape[-3:] == target.shape[-3:]:
         raise ValueError("input and target shapes must be the same. Got: {} and {}"
                          .format(input.shape, input.shape))
 
@@ -138,8 +142,8 @@ class CustomNativeDataset(Dataset):
         
         self.imagefiles = [glob.glob(os.path.join(folder, '*.*')) for folder in self.imagedir]
         self.labelfiles = [glob.glob(os.path.join(folder, '*.*')) for folder in self.labeldir]
-        self.imagefiles = [item for sublist in self.imagefiles for item in sublist]
-        self.labelfiles = [item for sublist in self.labelfiles for item in sublist]
+        self.imagefiles = natsorted([item for sublist in self.imagefiles for item in sublist])
+        self.labelfiles = natsorted([item for sublist in self.labelfiles for item in sublist])
         self.transforms = transforms
         assert len(self.imagefiles) == len(self.labelfiles)
         # print(self.imagefiles)
@@ -151,19 +155,38 @@ class CustomNativeDataset(Dataset):
  
     def __getitem__(self, idx):
         # print(self.imagefiles[idx], self.labelfiles[idx])
-        image = cv2.imread(self.imagefiles[idx], cv2.IMREAD_GRAYSCALE)
-        label = cv2.imread(self.labelfiles[idx], cv2.IMREAD_GRAYSCALE)
-        assert image.shape[:-2] == label.shape[:-2]
+        image = skimage.io.imread(self.imagefiles[idx])
+        label = skimage.io.imread(self.labelfiles[idx])
+        if image.shape != label.shape:
+            print(self.imagefiles[idx])
+            print(self.labelfiles[idx])
+        assert image.shape == label.shape
+        # print(image.shape, label.shape)
+
         if self.transforms is not None:
+            # image = np.transpose(image, (1, 2, 0))
+            # label = np.transpose(label, (1, 2, 0))
             transformed = self.transforms(image=image, mask=label)
             image = transformed['image']
             label = transformed['mask']
+            # image = np.transpose(image, (2, 0, 1))
+            # label = np.transpose(label, (2, 0, 1))
+        image = skimage.transform.resize(image, [64, 256, 256], order=5) # Bi-linear
+        label = skimage.transform.resize(label, [64, 256, 256], order=5) # Nearest neighbor
+        # image = skimage.exposure.equalize_adapthist(image, clip_limit=0.03)
+        p2, p98 = np.percentile(image, (2, 98))
+        image = skimage.exposure.rescale_intensity(image, in_range=(p2, p98))
+        # print(image.max(), image.min(), label.max(), label.min())
+        image = 255*(image)
+        label = 255*(label>0.5)
 
-        return kornia.image_to_tensor(image).float(), \
-               kornia.image_to_tensor(label).float()
+        
+        # print("After", image.shape, label.shape)
+        return torch.Tensor(image).float().unsqueeze_(0), \
+               torch.Tensor(label).float().unsqueeze_(0)
 
 
-class UNet(nn.Module):
+class VNet(nn.Module):
     """
     Architecture based on U-Net: Convolutional Networks for Biomedical Image Segmentation
     Link - https://arxiv.org/abs/1505.04597
@@ -196,7 +219,7 @@ class UNet(nn.Module):
             layers.append(Up(feats, feats // 2, bilinear))
             feats //= 2
 
-        layers.append(nn.Conv2d(feats, nb_class, kernel_size=1))
+        layers.append(nn.Conv3d(feats, nb_class, kernel_size=1))
         self.layers = nn.ModuleList(layers)
         self.output = nn.Tanh() #nn.ReLU(inplace=True)
 
@@ -224,11 +247,11 @@ class DoubleConv(nn.Module):
     ):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_ch),
+            nn.Conv3d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_ch),
+            nn.Conv3d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm3d(out_ch),
             nn.ReLU(inplace=True)
         )
 
@@ -238,7 +261,7 @@ class DoubleConv(nn.Module):
 
 class Down(nn.Module):
     """
-    Combination of MaxPool2d and DoubleConv in series
+    Combination of MaxPool3d and DoubleConv in series
     """
 
     def __init__(
@@ -248,7 +271,7 @@ class Down(nn.Module):
     ):
         super().__init__()
         self.net = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool3d(kernel_size=2, stride=2),
             DoubleConv(in_ch, out_ch)
         )
 
@@ -276,12 +299,12 @@ class Up(nn.Module):
                 nn.Upsample(scale_factor=2, 
                             mode="bilinear", 
                             align_corners=True),
-                nn.Conv2d(in_ch, 
+                nn.Conv3d(in_ch, 
                           in_ch // 2, 
                           kernel_size=1),
             )
         else:
-            self.upsample = nn.ConvTranspose2d(in_ch, 
+            self.upsample = nn.ConvTranspose3d(in_ch, 
                                                in_ch // 2, 
                                                kernel_size=2, 
                                                stride=2)
@@ -292,11 +315,13 @@ class Up(nn.Module):
         x1 = self.upsample(x1)
 
         # Pad x1 to the size of x2
-        diff_h = x2.shape[2] - x1.shape[2]
-        diff_w = x2.shape[3] - x1.shape[3]
+        diff_d = x2.shape[2] - x1.shape[2]
+        diff_h = x2.shape[3] - x1.shape[3]
+        diff_w = x2.shape[4] - x1.shape[4]
 
-        x1 = F.pad(x1, [diff_w // 2, diff_w - diff_w // 2, 
-                        diff_h // 2, diff_h - diff_h // 2])
+        x1 = F.pad(x1, [diff_d // 2, diff_d - diff_d // 2, 
+                        diff_h // 2, diff_h - diff_h // 2, 
+                        diff_w // 2, diff_w - diff_w // 2])
 
         # Concatenate along the channels axis
         x = torch.cat([x2, x1], dim=1)
@@ -309,22 +334,23 @@ class Model(pl.LightningModule):
         super(Model, self).__init__()
         self.hparams = hparams
         self.n_classes = n_classes
-        self.unet = UNet()
-        print(self.unet)
-        
+        self.vnet = VNet(
+            features=self.hparams.features, 
+            bilinear=self.hparams.bilinear,
+        )
+        print(self.vnet)
     def forward(self, x):
 
-        return self.unet(x)
+        return self.vnet(x)
 
     def training_step(self, batch, batch_nb):
         x, y = batch
         x = x / 255.0
         y = y / 255.0
         y_hat = self.forward(x)
-        # loss = F.cross_entropy(y_hat, y) if self.n_classes > 1 else \
-        #     F.binary_cross_entropy_with_logits(y_hat, y)
         loss = dice_loss(y_hat, y)
-        vis_images = torch.cat([x, y, y_hat], dim=-1)[:8]
+        vis_images = torch.cat([x, y, y_hat], dim=-1)#[:8]
+        vis_images = vis_images[:,:,32,:,:]
         grid = torchvision.utils.make_grid(vis_images, nrow=2, padding=0)
         self.logger.experiment.add_image('train_vis', grid, self.current_epoch)
         tensorboard_logs = {'train_loss': loss}
@@ -332,16 +358,15 @@ class Model(pl.LightningModule):
 
     def validation_step(self, batch, batch_nb):
         x, y = batch
+
         x = x / 255.0
         y = y / 255.0
         y_hat = self.forward(x)
-        # loss = F.cross_entropy(y_hat, y) if self.n_classes > 1 else \
-        #     F.binary_cross_entropy_with_logits(y_hat, y)
         loss = dice_loss(y_hat, y)
-        vis_images = torch.cat([x, y, y_hat], dim=-1)[:8]
+        vis_images = torch.cat([x, y, y_hat], dim=-1)#[:8]
+        vis_images = vis_images[:,:,32,:,:]
         grid = torchvision.utils.make_grid(vis_images, nrow=2, padding=0)
         self.logger.experiment.add_image('valid_vis', grid, self.current_epoch)
-        # loss = kornia.dice_loss(y_hat, y.long())
         return {'val_loss': loss}
 
     def validation_epoch_end(self, outputs):
@@ -353,25 +378,30 @@ class Model(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
 
     def __dataloader(self):
+        train_tfm = None
+        valid_tfm = None
         train_tfm = AB.Compose([
             # AB.ToFloat(), 
-            AB.Rotate(limit=30, border_mode=cv2.BORDER_CONSTANT, p=1.0),
-            AB.Resize(height=1024, width=1024, p=1.0),
-            AB.CropNonEmptyMaskIfExists(height=960, width=960, p=0.8), 
-            AB.RandomScale(scale_limit=(0.8, 1.2), p=0.8),
-            AB.Equalize(p=0.8),
-            AB.CLAHE(p=0.8),
+            # AB.Rotate(limit=20, border_mode=cv2.BORDER_CONSTANT, p=1.0),
+            # AB.Resize(height=512, width=512, p=1.0), 
+            # AB.CropNonEmptyMaskIfExists(height=480, width=480, p=0.8), 
+            # AB.RandomScale(scale_limit=(0.8, 1.2), p=0.8),
+            # AB.Equalize(p=0.8),
+            # AB.CLAHE(p=0.8),
             AB.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.8),
             AB.RandomGamma(gamma_limit=(80, 120), p=0.8),
             AB.GaussianBlur(p=0.8),
             AB.GaussNoise(p=0.8),
-            AB.Resize(width=self.hparams.shape, height=self.hparams.shape, p=1.0),
+            # AB.Resize(width=self.hparams.dimy, height=self.hparams.dimx, p=1.0),
             # AB.ToTensor(),
         ])
         valid_tfm = AB.Compose([
             # AB.ToFloat(), 
-            AB.Resize(width=self.hparams.shape, height=self.hparams.shape),
+            # AB.Equalize(p=1.0),
+            # AB.CLAHE(p=1.0),
+            # AB.Resize(width=self.hparams.dimy, height=self.hparams.dimx),
         ])
+
         train_ds = CustomNativeDataset(imagedir=train_image_dir, 
                                        labeldir=train_label_dir, 
                                        train_or_valid='train', 
@@ -382,13 +412,13 @@ class Model(pl.LightningModule):
                                        transforms=valid_tfm)
 
         train_loader = DataLoader(train_ds, 
-            num_workers=8, 
+            num_workers=self.hparams.num_workers, 
             batch_size=self.hparams.batch_size, 
             pin_memory=True, 
             shuffle=True
         )
         valid_loader = DataLoader(valid_ds, 
-            num_workers=8, 
+            num_workers=self.hparams.num_workers, 
             batch_size=self.hparams.batch_size, 
             pin_memory=True, 
             shuffle=False
@@ -457,16 +487,20 @@ def main(hparams):
 if __name__ == '__main__':
     # print('Start')
     parser = ArgumentParser(add_help=False)
-    parser.add_argument('--shape', type=int, default=256)
+    parser.add_argument('--dimx', type=int, default=256)
+    parser.add_argument('--dimy', type=int, default=256)
+    parser.add_argument('--dimz', type=int, default=64)
     parser.add_argument('--lgdir', type=str, default='lightning_logs')
+    parser.add_argument('--seed', type=int, default=42)
     parser.add_argument("--gpus", type=int, default=-1, help="number of available GPUs")
     parser.add_argument('--distributed-backend', type=str, default='dp', choices=('dp', 'ddp', 'ddp2'),
                         help='supports three options dp, ddp, ddp2')
     parser.add_argument('--use_amp', action='store_true', help='if true uses 16 bit precision')
-    parser.add_argument("--batch_size", type=int, default=32, help="size of the batches")
+    parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
+    parser.add_argument("--num_workers", type=int, default=4, help="size of the workers")
     parser.add_argument("--lr", type=float, default=0.0001, help="learning rate")
     parser.add_argument("--nb_layer", type=int, default=5, help="number of layers on u-net")
-    parser.add_argument("--features", type=float, default=64, help="number of features in first layer")
+    parser.add_argument("--features", type=float, default=32, help="number of features in first layer")
     parser.add_argument("--bilinear", action='store_true', default=False,
                         help="whether to use bilinear interpolation or transposed")
     parser.add_argument("--grad_batches", type=int, default=1, help="number of batches to accumulate")
@@ -475,5 +509,14 @@ if __name__ == '__main__':
     
     parser = Model.add_model_specific_args(parser)
     hparams = parser.parse_args()
+
+    random.seed(hparams.seed)
+    os.environ['PYTHONHASHSEED'] = str(hparams.seed)
+    np.random.seed(hparams.seed)
+    torch.manual_seed(hparams.seed)
+    torch.cuda.manual_seed(hparams.seed)
+    torch.cuda.manual_seed_all(hparams.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     main(hparams)
